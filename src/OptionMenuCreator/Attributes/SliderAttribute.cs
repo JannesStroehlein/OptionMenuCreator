2d41373c3ccb7142946b038726f33a8ef6459f15
Jannes Ströhlein <48127946+JannesStroehlein@users.noreply.github.com>
2020-05-21 13:18:28 +0200

    Added Attributes

---
+++ src/OptionMenuCreator/Attributes/SliderAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionMenuCreator.Attributes
+{
+    public class SliderAttribute : Attribute
+    {
+        public double minValue { get; private set; }
+        public double maxValue { get; private set; }
+
+        public SliderAttribute(double minValue, double maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+    }
+}
---
+++ src/OptionMenuCreator/Attributes/TextboxAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionMenuCreator.Attributes
+{
+    public class TextboxAttribute : Attribute
+    {
+        public bool multiLine { get; private set; }
+        public int maxChars { get; private set; }
+        public TextboxAttribute(bool multiLine, int maxChars)
+        {
+            this.multiLine = multiLine;
+            this.maxChars = maxChars;
+        }
+    }
+}
