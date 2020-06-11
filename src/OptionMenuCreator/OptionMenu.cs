2476a7c608d367ddb7da2021f907fe4f50a8a43d
Jannes Ströhlein <48127946+JannesStroehlein@users.noreply.github.com>
2020-06-11 15:36:43 +0200

    V0.3 Basic Functionality
    - Added support for
      - string, char
      - int, double, float
      - bool
    - Cleaned up the code
    - removed debug output
    - made to window scrollable
    - minor bugfixes

--- src/OptionMenuCreator/OptionMenu.cs
+++ src/OptionMenuCreator/OptionMenu.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Threading;
 using OptionMenuCreator.Attributes;
 using OptionMenuCreator.Window;
@@ -21,6 +22,10 @@ namespace OptionMenuCreator
         private OptionMenuControll menu;
         [NotEditable]
         private Thread windowThread;
+        [NotEditable]
+        private Type[] typeWhiteList = { typeof(string), typeof(int), typeof(double), typeof(float), typeof(bool), typeof(char) };
+        [NotEditable]
+        private const int OptionHeight = 30;
 
         public OptionMenu()
         {
@@ -36,14 +41,17 @@ namespace OptionMenuCreator
         private Label CreateLabelWithText(String Text)
         {
             Label ret = new Label();
+            ret.Height = OptionHeight;
             ret.Content = Text;
+            ret.VerticalAlignment = VerticalAlignment.Center;
             return ret;
         }
         private void ThreadMethod()
         {
             this.window = new System.Windows.Window();
-            this.window.Height = 450;
-            this.window.Width = 300;
+            this.window.Height = 350;
+            this.window.Width = 400;
+            this.window.Title = this.Title;
             this.menu = new OptionMenuControll();
             this.BakeMenu();
             this.window.Content = this.menu;
@@ -57,25 +65,128 @@ namespace OptionMenuCreator
             {
                 if (info.CanWrite & info.CanWrite)
                 {
+                    bool isEditable = true;
                     foreach (Attribute attrib in info.GetCustomAttributes<Attribute>())
                     {
                         if (attrib.GetType() == typeof(NotEditableAttribute))
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
+                            isEditable = false;
                             break;
                         }
                         else
-                            Console.ForegroundColor = ConsoleColor.Green;
+                            isEditable = true;                     
                     }
-                    Console.WriteLine(info.Name);
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                    Console.WriteLine(info.Name);
-                }            
+                    if (isEditable)
+                    {
+                        bool isWhiteListed = false;
+                        foreach (Type test in this.typeWhiteList)
+                        {
+                            if (test == info.PropertyType)
+                            {
+                                isWhiteListed = true;
+                                break;
+                            }
+                        }
+                        if (!isWhiteListed)
+                            continue;
+                    }
+                    else
+                        continue;
+                    // Option Name
+                    this.menu.Left.Children.Add(this.CreateLabelWithText(info.Name));
+
+                    // Option Input
+                    if (info.PropertyType == typeof(float) | info.PropertyType == typeof(double) | info.PropertyType == typeof(int)) //Numeric Input
+                    {
+                        if (info.GetCustomAttribute<SliderAttribute>() != null)
+                        {
+                            SliderAttribute attribute = info.GetCustomAttribute<SliderAttribute>();
+                            Slider slider = new Slider();
+                            slider.Height = OptionHeight;
+                            slider.ToolTip = 0;
+                            if (info.GetValue(this) != null)
+                                slider.Value = Convert.ToDouble(info.GetValue(this)); // Set current value
+                            slider.VerticalAlignment = VerticalAlignment.Center;
+                            slider.ValueChanged += delegate(object sender, RoutedPropertyChangedEventArgs<double> e)
+                            {
+                                slider.ToolTip = slider.Value;
+                                if (info.PropertyType == typeof(int))
+                                    info.SetValue(this, Convert.ToInt32(slider.Value));
+                                else
+                                    info.SetValue(this, slider.Value);
+                            };
+                            slider.Minimum = attribute.minValue;
+                            slider.Maximum = attribute.maxValue;
+                            if (info.PropertyType == typeof(int))
+                            {
+                                slider.TickFrequency = 1;
+                                slider.IsSnapToTickEnabled = true;
+                            }
+                            this.menu.Right.Children.Add(slider);
+                        }
+                        else
+                        {
+                            TextBox textBox = new TextBox();
+                            textBox.Height = OptionHeight;
+                            if (info.GetValue(this) != null)
+                                textBox.Text = info.GetValue(this).ToString(); // Set current value
+                            textBox.VerticalAlignment = VerticalAlignment.Center;
+                            textBox.PreviewTextInput += delegate (object sender, TextCompositionEventArgs e)
+                            {
+                                Regex _regex = new Regex("[^0-9.,-]+");
+                                e.Handled = _regex.IsMatch(e.Text);
+                            };
+                            textBox.TextChanged += delegate (object sender, TextChangedEventArgs e)
+                            {
+                                if (info.PropertyType == typeof(double))
+                                    info.SetValue(this, Convert.ToDouble(textBox.Text));
+                                else if (info.PropertyType == typeof(float))
+                                    info.SetValue(this, float.Parse(textBox.Text));
+                                else if (info.PropertyType == typeof(int))
+                                    info.SetValue(this, Convert.ToInt32(textBox.Text));
+                            };
+                            this.menu.Right.Children.Add(textBox);
+                        }
+                    }
+                    else if (info.PropertyType == typeof(string) | info.PropertyType == typeof(char)) // Text Input
+                    {
+                        TextBox textBox = new TextBox();
+                        textBox.Height = OptionHeight;
+                        if (info.GetValue(this) != null)
+                            textBox.Text = info.GetValue(this).ToString(); // Set current value
+                        textBox.TextChanged += delegate (object sender, TextChangedEventArgs e)
+                        {
+                            info.SetValue(this, textBox.Text);
+                        };
+                        if (info.GetCustomAttribute<TextboxAttribute>() != null)
+                        {
+                            TextboxAttribute attribute = info.GetCustomAttribute<TextboxAttribute>();
+                            textBox.MaxLength = attribute.maxChars;
+                            if (attribute.multiLine)
+                                textBox.MaxLines = 999;
+                        }
+                        else
+                            textBox.MaxLines = 1;
+                        this.menu.Right.Children.Add(textBox);
+                    }
+                    else if (info.PropertyType == typeof(bool))
+                    {
+                        CheckBox checkBox = new CheckBox();
+                        checkBox.Height = OptionHeight;
+                        if (info.GetValue(this) != null)
+                            checkBox.IsChecked = (bool)info.GetValue(this); // Set current value
+                        checkBox.VerticalAlignment = VerticalAlignment.Center;
+                        checkBox.HorizontalAlignment = HorizontalAlignment.Center;
+                        checkBox.Checked += delegate (object sender, RoutedEventArgs e)
+                        {
+                            info.SetValue(this, checkBox.IsChecked);
+                        };
+                        this.menu.Right.Children.Add(checkBox);
+                    }
+                    else
+                        this.menu.Right.Children.Add(this.CreateLabelWithText(" ")); // Add dummy
+                }       
             }
-            Console.ResetColor();
         }
     }
 }
--- src/Test/Program.cs
+++ src/Test/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OptionMenuCreator;
 using OptionMenuCreator.Attributes;
 
@@ -21,9 +17,15 @@ namespace Test
     {
         [Slider(0, 1)]
         public double d1 { get; set; }
+        [Slider(0, 10)]
+        public int i1 { get; set; }
 
         [Textbox(false, 16)]
         public string t1 { get; set; }
+        [Textbox(true, 140)]
+        public string t2 { get; set; }
+        public int i2 { get; set; }
+        public bool b1 { get; set; }
 
         public Menu() : base()
         {
